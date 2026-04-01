40ce0479a441aae2526584917a8eff397b19c018
tilt <95900257+tiltis@users.noreply.github.com>
2026-04-01 15:40:51 +0900

    L1L0 / iCAM06 / Kwon et al.

--- main.m
+++ main.m
@@ -1,6 +1,8 @@
-pathname = 'D:\24_year_vietnam\실험 결과\102_test_sample';
+%%
+tic
+pathname = 'C:\Users\student\Desktop\[code_final]_2020_AS_contrast_sensitivity';
 
-[filename, pathname, index] = uigetfile ({'*.jpg'},'Pick a Image file', pathname,'MultiSelect','on');
+[filename, pathname, index] = uigetfile ({'*.*'},'Pick a Image file', pathname,'MultiSelect','on');
 
 
 if (index == 0)
@@ -14,37 +16,29 @@ else
     imageCount = 1;
 end
 
+ag.Proposed.isSelected = true;
+ag.iCAM06.isSelected = true;
+ag.L1L0.isSelected = true;
+ag.GLW.isSelected = false;
+
+
 % 저장 폴더명, 년,월,일.시.분.초.
 dt = datestr(now,'yyyy.mm.dd.HH.MM.SS');
 % 폴더 생성
-saveDir = ['.\result\' dt ];
-
+saveDir = ['.\result\' dt '\'];
 try
     mkdir(saveDir);
 catch
     errordlg('폴더생성 실패');
 end
 
+
 for index = 1:imageCount
     if iscell(filename)
-         %hdr = hdrimread([pathname, filename{index}]); % default
-        hdr = imread([pathname, filename{index}]);
+        Run(pathname, filename{index}, 1, false, true, ag, saveDir);
     else
-        hdr = imread([pathname, filename]);
+        Run(pathname, filename, 1, false, true, ag, saveDir);
     end
-    
-    hdr = double(hdr)/255;
-    hdr = hdr.^(1./2.2);
-    
-    %Reinhard 2012 version.
-    [Reinhard2012]= ReinhardTMO(hdr,0,0);
-
-    if iscell(filename)
-        imwrite(Reinhard2012,[saveDir '\' filename{index}(1:end-4),'_Reinhard2012.tif']);
-    else
-        imwrite(Reinhard2012,[saveDir '\' filename(1:end-4),'_Reinhard2012.tif']);
-    end
-
     fprintf('진행상황 %d/%d\n',index,imageCount);
 end
-
+toc
